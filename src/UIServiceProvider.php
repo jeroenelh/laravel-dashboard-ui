eb65706d164e369be9233ffbb4807d395173feda
Jeroen el Hahaoui <jeroen.el.hahaoui@maxict.nl>
2023-08-10 23:08:00 +0200

    Init

---
+++ src/UIServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Microit\DashboardUI;
+
+use Illuminate\Support\ServiceProvider;
+
+class UIServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations/');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'ui');
+    }
+
+    public function register(): void
+    {
+    }
+}
